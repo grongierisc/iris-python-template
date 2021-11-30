59a089801323135f56d83e10117b2439e853bc71
grongierisc <guillaume.rongier@intersystems.com>
2021-11-30 17:11:52 +0100

    add gateway

---
+++ src/Python/gateway/Address.py
@@ -0,0 +1,7 @@
+class Address:
+
+    def __init__(self, street="One Memorial Drive", city="Cambridge", state="MA", zip="02142"):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zip = zip
