2e61ce9d4901afefd3d33c44d8ebc51f7a53988e
grongierisc <guillaume.rongier@intersystems.com>
2021-12-02 15:54:46 +0100

    update demo

--- src/Python/native/demo.py
+++ src/Python/native/demo.py
@@ -1,18 +1,11 @@
 import irisnative
 
-# Modify connection info based on your environment
-host = "localhost"
-port = 1972
-namespace = "USER"
-username = "_SYSTEM"
-password = "SYS"
-
 # create database connection and IRIS instance
 connection = irisnative.createConnection("localhost", 1972, "USER", "superuser", "SYS", sharedmemory = False)
 myIris = irisnative.createIris(connection)
 
 # classMethod
-passenger = myIris.classMethodObject("Titanic.Table.Passenger","%OpenId",1)
+passenger = myIris.classMethodObject("Titanic.Table.Passenger","%OpenId",2674)
 print(passenger.get("name"))
 
 # global
