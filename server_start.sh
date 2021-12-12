eb0c19c326d51c578c7c002116e14cf857dcf21d
grongierisc <guillaume.rongier@intersystems.com>
2021-12-12 16:00:59 +0100

    Update path for flask

--- server_start.sh
+++ server_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ${PYTHON_SRC_PATH}
+cd ${SRC_PATH}/src/Python/flask
 
-${PYTHON_PATH} -m gunicorn --bind "0.0.0.0:8080" wsgi:app -w 4 2>&1
+${PYTHON_PATH} -m gunicorn --bind "0.0.0.0:8080" wsgi:app 2>&1
 
 /home/irisowner/.local/bin/jupyter-notebook --no-browser --port=8888 --ip 0.0.0.0 --notebook-dir=/irisdev/app/src/Python/notebooks --NotebookApp.token='' --NotebookApp.password='' &
 
